1c0074da85afbd8150b9d08ae58844d717b213b6
Marco Braun <marco.braun@exxeta.com>
2022-12-02 18:23:02 +0100

    feat: add user entry

---
+++ controller/entry.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"rehoboam/helper"
+	"rehoboam/model"
+)
+
+func AddEntry(context *gin.Context) {
+	var input model.Entry
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := helper.CurrentUser(context)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	input.UserId = user.ID
+
+	savedEntry, err := input.Save()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"data": savedEntry})
+}
--- model/entry.go
+++ model/entry.go
@@ -2,9 +2,18 @@ package model
 
 import (
 	"gorm.io/gorm"
+	"rehoboam/database"
 )
 
 type Entry struct {
 	gorm.Model
 	UserId uint
+}
+
+func (entry *Entry) Save() (*Entry, error) {
+	err := database.Connect().Create(&entry).Error
+	if err != nil {
+		return &Entry{}, err
+	}
+	return entry, nil
 }
