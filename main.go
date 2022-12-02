ef5ebe696d64672eae3a9b42e673d6f8fbeefd74
Marco Braun <marco.braun@exxeta.com>
2022-12-02 18:27:10 +0100

    feat: add protected routes

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"rehoboam/controller"
 	"rehoboam/database"
+	"rehoboam/middleware"
 	"rehoboam/model"
 )
 
@@ -33,8 +34,13 @@ func serveApplication() {
 	router := gin.Default()
 
 	publicRoutes := router.Group("/auth")
-	publicRoutes.POST("/signUp", controller.SignUp)
-	publicRoutes.POST("/signIn", controller.SignIn)
+	publicRoutes.POST("/signup", controller.SignUp)
+	publicRoutes.POST("/signin", controller.SignIn)
+
+	protectedRoutes := router.Group("/api/v1")
+	protectedRoutes.Use(middleware.JWTAuthMiddleware())
+	protectedRoutes.POST("/entry", controller.AddEntry)
+	protectedRoutes.GET("/entry", controller.GetAllEntries)
 
 	router.Run(":8000")
 	fmt.Println("Server running on port 8000")
